2bb6ecad5eab2aedcecbc1e71268986239fcaf7a
Vaghasiya Vikas <vikas@socialpilot.co>
2020-05-01 22:17:57 +0530

    -- Delete files

--- src/shared/carousel.js
+++ src/shared/carousel.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import Slider from 'react-slick'
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 import 'react-multi-carousel/lib/styles.css'
-import { Col, Icon } from 'antd'
-//import blogImg from '../assets/images/Blog_img01.png'
+import { Col } from 'antd'
 import blogImg from '../assets/images/testimonial-user.jpg'
 
 const settings = {
