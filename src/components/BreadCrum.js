852d3d32387c456e509cf53583e196be2bb38698
Vaghasiya Vikas <vikas@socialpilot.co>
2020-05-16 23:49:32 +0530

    -- create design for more categories

--- src/components/BreadCrum.js
+++ src/components/BreadCrum.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { navigate,parsePath } from 'gatsby'
-import { Tabs } from 'antd'
+import { Popover, Tabs } from 'antd'
 import { Location } from '@reach/router';
 
 
@@ -13,6 +13,52 @@ class CategoryTabs extends React.Component {
   handleChange = (e) =>{
     return navigate(e)
   }
+
+  content = () =>(
+    <div className='breadcrum-more-content'>
+      <div className='parent-container'>
+        <h3>Parent</h3>
+        <ul>
+          <li><a>child 1</a></li>
+          <li><a>child 2</a></li>
+          <li><a>child 2</a></li>
+        </ul>
+      </div>
+      <div className='parent-container'>
+        <h3>Parent</h3>
+        <ul>
+          <li><a>child 1</a></li>
+          <li><a>child 2</a></li>
+          <li><a>child 2</a></li>
+        </ul>
+      </div>
+      <div className='parent-container'>
+        <h3>Parent</h3>
+        <ul>
+          <li><a>child 1</a></li>
+          <li><a>child 2</a></li>
+          <li><a>child 2</a></li>
+        </ul>
+      </div>
+      <div className='parent-container'>
+        <h3>Parent</h3>
+        <ul>
+          <li><a>child 1</a></li>
+          <li><a>child 2</a></li>
+          <li><a>child 2</a></li>
+        </ul>
+      </div>
+      <div className='parent-container'>
+        <h3>Parent</h3>
+        <ul>
+          <li><a>child 1</a></li>
+          <li><a>child 2</a></li>
+          <li><a>child 2</a></li>
+        </ul>
+      </div>
+    </div>
+  )
+
   render() {
     const { edges } = this.props;
     return (
@@ -22,6 +68,15 @@ class CategoryTabs extends React.Component {
             className={`category-tabs ${this.props.noTop ? 'no-top-space-tabs' : ''}`}
             onChange={this.handleChange}
             activeKey={location?.location.pathname.slice(-1) === '/' ? location?.location.pathname : `${location?.location.pathname}/`}
+            tabBarExtraContent={<div className='breadcrum-more'>
+              <Popover
+                content={this.content()}
+                overlayClassName='breadcrum-more-overlay'
+                placement='bottomLeft'
+              >
+              <a className='para-text'>Get More</a>
+              </Popover>
+            </div>}
           >
             <TabPane tab="All Categories" key="/blog/"/>
             {edges?.map(({ node: { fields, frontmatter } }) =>
