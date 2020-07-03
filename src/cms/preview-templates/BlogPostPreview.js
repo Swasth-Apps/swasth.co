00af73e6468a04f88bb012c144da5b79659ba58c
Vaghasiya Vikas <vikas@socialpilot.co>
2020-07-03 20:58:45 +0530

    -- change

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -4,9 +4,9 @@ import BlogPostTemplate from '../../templates/blog-post'
 const BlogPostPreview = ({ entry, widgetFor }) => (
   <BlogPostTemplate
     content={widgetFor('body')}
-    description={entry.getIn(['data', 'description'])}
-    tags={entry.getIn(['data', 'tags'])}
-    title={entry.getIn(['data', 'title'])}
+    description={entry.getIn(['data', 'description']).toJS()}
+    tags={entry.getIn(['data', 'tags']).toJS()}
+    title={entry.getIn(['data', 'title']).toJS()}
   />
 )
 
