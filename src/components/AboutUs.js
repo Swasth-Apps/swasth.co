2193f20b6ef178f33875e63a40bdfb246c83dabc
gana <gana@swasth.co>
2020-11-01 20:40:54 -0800

    UI Changes

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -35,37 +35,36 @@ const Home = (props) => {
             <section className='about-us-banner-section'>
                 <div className="about-us-banner-content">
                     <h1>We are bringing evidence-based behavioral healthcare to everyone.</h1>
-                    <h4>
+                    {/*<h4>
                         Join us in building a healthy world
                     </h4>
-                    {/*<Button className="-btn-primary"> See open positions </Button>   */}
+                    <Button className="-btn-primary"> See open positions </Button>   */}
                 </div>
             </section>
             <section>
                 <div className="about-us-tabs-container">
                     <div className="about-us-tabs">
-                        <h4 onClick={()=>findIdAndScroll("whoWeAre")}><a>Who We Are</a></h4>
-                        <h4 onClick={()=>findIdAndScroll("probSol")}><a>Our Mission</a></h4>
+                        {/*<h4 onClick={()=>findIdAndScroll("whoWeAre")}><a>Who We Are</a></h4> */}
+                        <h4 onClick={()=>findIdAndScroll("missionStatement")}><a>Our Mission</a></h4>
                         <h4 onClick={()=>findIdAndScroll("leaderTeam")}><a>Leadership Team</a></h4>
-                        <h4 onClick={()=>findIdAndScroll("joinTeam")}><a>Open Position</a></h4>
+                        {/*<h4 onClick={()=>findIdAndScroll("joinTeam")}><a>Open Position</a></h4>*/}
                     </div>
                 </div>
             </section>
             <section>
-                <div className="about-us-mission-container" id="whoWeAre">
+                <div className="about-us-mission-container" id="missionStatement">
                     <div className="mission-content">
-                        <h5>Why We Work</h5>
+                        <h5>Mission Statement</h5>
                         <h3>
-                            Our mission is to transform behavioral health care through technology with a human touch, to
-                            help people feel emotionally healthy at work and at home.
+                            Our mission is to transform behavioral health care through technology, evidence-based therapy, guided by experts and coaches to empower people improve their mental-health. 
                         </h3>
                     </div>
                     <div className="mission-img">
-                        <img src={require("../assets/images/mission.jpg")}/>
+                        <img src={require("../assets/images/aboutus-mission.png")}/>
                     </div>
                 </div>
             </section>
-            <section className="about-us-prob-sol" id="probSol">
+            {/*<section className="about-us-prob-sol" id="probSol">
                 <div className="problem-section comm-section">
                     <div className="inner">
                         <h5 className="title">The Problem</h5>
@@ -78,7 +77,7 @@ const Home = (props) => {
                     <div className="inner">
                         <h5 className="title">The Solution</h5>
                         <h3>We are revolutionizing how people find and receive care.</h3>
-                        <h5>In fact, Lyra helps people get care 21x faster and experience 3x better outcomes, which
+                        <h5>In fact, Swasth helps people get care 21x faster and experience 3x better outcomes, which
                             means they feel better, faster.</h5>
                     </div>
                 </div>
@@ -96,11 +95,11 @@ const Home = (props) => {
                             <img src={"/img/purple_yellow_gradient.jpg"}/>
                             <div className="content">
                                 <h4>We are mission-driven</h4>
-                                <p>No matter your role at Lyra, you’ll hear real-life stories of how your work is
+                                <p>No matter your role at Swasth, you’ll hear real-life stories of how your work is
                                     improving people’s lives and see it at scale in the results we share with our
                                     customers. We are proud of what we’re doing.</p>
                                 <h4>In Practice</h4>
-                                <p>People often tell us Lyra is life-changing and we share those stories in daily
+                                <p>People often tell us Swasth is life-changing and we share those stories in daily
                                     standups, Slack channels, and more.</p>
                             </div>
                         </div>
@@ -108,11 +107,11 @@ const Home = (props) => {
                             <img src={"/img/purple_yellow_gradient.jpg"}/>
                             <div className="content">
                                 <h4>We are mission-driven</h4>
-                                <p>No matter your role at Lyra, you’ll hear real-life stories of how your work is
+                                <p>No matter your role at Swasth, you’ll hear real-life stories of how your work is
                                     improving people’s lives and see it at scale in the results we share with our
                                     customers. We are proud of what we’re doing.</p>
                                 <h4>In Practice</h4>
-                                <p>People often tell us Lyra is life-changing and we share those stories in daily
+                                <p>People often tell us Swasth is life-changing and we share those stories in daily
                                     standups, Slack channels, and more.</p>
                             </div>
                         </div>
@@ -120,21 +119,21 @@ const Home = (props) => {
                             <img src={"/img/purple_yellow_gradient.jpg"}/>
                             <div className="content">
                                 <h4>We are mission-driven</h4>
-                                <p>No matter your role at Lyra, you’ll hear real-life stories of how your work is
+                                <p>No matter your role at Swasth, you’ll hear real-life stories of how your work is
                                     improving people’s lives and see it at scale in the results we share with our
                                     customers. We are proud of what we’re doing.</p>
                                 <h4>In Practice</h4>
-                                <p>People often tell us Lyra is life-changing and we share those stories in daily
+                                <p>People often tell us Swasth is life-changing and we share those stories in daily
                                     standups, Slack channels, and more.</p>
                             </div>
                         </div>
 
                     </div>
                 </div>
-            </section>
+            </section> */}
             <section className="about-us-leadership">
                 <h3 id="leaderTeam">Our Leadership team</h3>
-                <p>Our technologists, mental health experts, and business leaders driving the strategy behind Lyra.</p>
+                {/*<p>Our technologists, mental health experts, and business leaders driving the strategy behind Swasth.</p>  */}
                 <div className="leader-section">
                     <div className="user">
                         <img src={require("../assets/images/gana.png")}/>
@@ -157,8 +156,8 @@ const Home = (props) => {
                 { <div className="user">
                     <img src={require("../assets/images/brian.jpg")}/>
                         <p className="name"> Brian Thomas </p>
-                        <p className="title">COO & Advisor</p>
-                        <p>Brian supports operations and finance at Swasth. Brian spent fourteen years as a corporate executive at Equinix where he served as Chief of Staff
+                        <p className="title">Advisor</p>
+                        <p>Brian spent fourteen years as a corporate executive at Equinix where he served as Chief of Staff
                             in the Office of the CEO, Vice President of Global Corporate Planning, and Executive Sponsor for Corporate Sustainability and Leadership
                             Development. Before Equinix, Brian was an investment banker with Citi for nine years. He received an M.Phil. in the Economics from the University of Cambridge and a BA in from Willamette University.</p>
                     </div> }
@@ -167,16 +166,16 @@ const Home = (props) => {
             <section className="about-us-join-team">
                 <div id="joinTeam">
                     <p>Ready to join our team ?</p>
-                    <Button className="-btn-primary"> See open positions </Button>
+                    <a href="mailto:jobs@swasth.co"> <Button className="-btn-primary"> Email Us </Button></a>
                 </div>
             </section>
             <section className="about-us-contact-us" >
                 <div className="support">
                     <h3>
-                        Member Support
+                        Members
                     </h3>
                     <p>
-                        Access care <a>online</a> or contact our care team at <a>care@lyrahealth.com</a> and <a>(877) 505–7147</a>.
+                        Contact our support team at <a>apps-help@swasth.co</a>.
                     </p>
                 </div>
                 <div className="support">
@@ -184,7 +183,7 @@ const Home = (props) => {
                         Sales
                     </h3>
                     <p>
-                        Contact our partnerships team at <a>partners@lyrahealth.com</a> and (800) 505-5972..
+                        Contact our partnerships team at <a>info@swasth.co.</a>
                     </p>
                 </div>
                 <div className="support">
@@ -192,7 +191,7 @@ const Home = (props) => {
                         Providers
                     </h3>
                     <p>
-                        Learn more about <a>how we work with providers</a> and contact the provider relations team at <a>providers@lyrahealth.com.</a>
+                        Contact the provider relations team at <a>providers@swasth.co.</a>
                     </p>
                 </div>
                 <div className="support">
@@ -200,7 +199,7 @@ const Home = (props) => {
                         Press
                     </h3>
                     <p>
-                        For press and media inquires, please contact us at <a>press@lyrahealth.com.</a>
+                        For press and media inquires, please contact us at <a>media@swasth.co.</a>
                     </p>
                 </div>
             </section>
