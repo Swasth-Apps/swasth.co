31cd0174a2b982a53e398fc616c438381cece0cb
vaghasiyavikas <vikasvaghasiya1212@gmail.com>
2021-02-12 13:58:21 +0530

    -- Updated images for programs

--- src/helper/programs.js
+++ src/helper/programs.js
@@ -37,7 +37,7 @@ export const helpData = [
             {
                 title:"Coping IBS symptoms with CBT",
                 "description":"This is a program based on Cognitive Behavioral Therapy for people suffering from Irritable Bowel Syndrome(IBS).",
-                "image":"What_is_IBS.png",
+                "image":"What_is_IBS.jpg",
                 "slug":"coping-ibs-symptoms-with-cbt"
             }
         ],
@@ -67,7 +67,7 @@ export const helpData = [
             {
                 title:"CBT for Panic Disorder",
                 "description":"<p>Panic disorder involves recurrent unexpected panic attacks and concerns about an anticipated increase in the number of future panic attacks. Thus the individual changes his or her behavior in maladaptive ways because of the panic attacks (e.g., avoidance of exercise or of unfamiliar locations). </p><p>Characteristically, the attacks are marked by abrupt surges of intense fear or intense discomfort that reach a peak within minutes, accompanied by physical and/or cognitive symptoms. In order to be categorized as having limited - symptom attack, the individual experiences fewer than four symptoms (enlisted the next section).</p><p></p><p> Panic attacks could arise due to a number of reasons such as, in response to a typically feared object or situation, or even sometimes unexpected, for no apparent reason.</p>",
-                "image":"Thoughts_Or_Feelings.png",
+                "image":"Thoughts_Or_Feelings.jpg",
                 "slug":"cbt-for-panic-disorder"
             }
         ],
@@ -82,17 +82,17 @@ export const helpData = [
             {
                 title:"Dealing with Chronic Pain with CBT",
                 "description":"<p>This is a program built for people suffering from Chronic Pain to help them heal with Cognitive Behavioral Therapy(CBT).</p>",
-                "image":"Causes_of_Chronic_Pain.png",
+                "image":"Causes_of_Chronic_Pain.jpg",
                 "slug":"dealing-with-chronic-pain-with-cbt"
             },{
                 title:"Positive Psychology",
                 "description":"<p>This program is equipped with vast positive psychology information and effective skills that can help you at any point of life with anything - cultivating strengths, being happy, being optimistic and building strong relationships.</p>",
-                "image":"Positive-Psychology.png",
+                "image":"Positive-Psychology.jpg",
                 "slug":"positive-psychology"
             },{
                 title:"Self Esteem",
                 "description":"<p dir=\"ltr\">Words like “self-image,” “self perception,” “self-concept”, “self - esteem” are all related to the individual being. However, it is first essential to understand their meaning. All these terms refer to the way we view and think about ourselves. </p><p dir=\"ltr\">We, social beings, possess the tendency to set standards of value or perhaps measures of worth for ourselves or aspects of ourselves.Thus self - esteem coincides with the way we perceive and think about ourselves and the level of worthiness at which we consider ourselves as.</p><p dir=\"ltr\">As a concept, self-esteem refers to a realistic, appreciative opinion we form for ourselves. Realistic involves knowing the truth about oneself, acknowledging one’s own strengths, weaknesses, temper and other qualities. </p><p dir=\"ltr\">On the other hand, Appreciative refers to our general perspective about ourselves. For example, our friends appreciate and praise us, looking beyond our faults. This is exactly what appreciative means.</p>",
-                "image":"career-value.png",
+                "image":"career-value.jpg",
                 "slug":"self-esteem"
             },{
                 title:"The Science of Gratitude and Well-Being",
@@ -116,7 +116,7 @@ export const helpData = [
             {
                 title:"CBT for Bipolar Disorder",
                 "description":"<p>Bipolar disorder comes under the mood disorder spectrum and refers to the experience of abnormal moods or exaggerated mood swings. The characteristic nature of this condition involves experiencing extremely “high” moods or manic episodes where one becomes extremely euphoric or elated, and experiencing extremely “low” moods or depressive episodes where one becomes extremely sad and finds it difficult to experience pleasure.</p><p></p><p>These episodes can range from mild to severe and affect social, occupational and personal functioning. However, it is important to remember that some people may experience different patterns associated with their disorder. For example, some people may experience only one episode of mania but more frequent episodes of depression and other such variations.</p>",
-                "image":"Bipolar_coexisting_with_other_conditions.png",
+                "image":"Bipolar_coexisting_with_other_conditions.jpg",
                 "slug":"cbt-for-bipolar-disorder"
             }
         ],
@@ -130,7 +130,7 @@ export const helpData = [
             {
                 title:"CBT for Addiction and Substance Use",
                 "description":"This is a program designed for people suffering from addiction and substance use disorders. It uses Cognitive Behavioral Therapy(CBT) to alleviate the dysfunctional urges and bring about more appropriate behaviors.",
-                "image":"About_Addiction_and_Substance_Use.png",
+                "image":"About_Addiction_and_Substance_Use.jpg",
                 "slug":"cbt-for-addiction-and-substance-use"
             }
         ],
