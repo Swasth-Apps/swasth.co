49c55918250bad325a43a12c2bf366510223c591
vaghasiyavikas <vikasvaghasiya1212@gmail.com>
2021-03-05 10:47:06 +0530

    -- Changes

--- src/pages/HIPAA-Business-Associate-Addendum.js
+++ src/pages/HIPAA-Business-Associate-Addendum.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import PDF from '../shared/HIPAA-Business-Associate-Addendum.pdf'
 
-
+if (typeof window === 'undefined') {
+    global.window = {
+        location: {}
+    }
+}
 class HomeIndex extends React.Component {
     render() {
         if (typeof window !== `undefined`) {
