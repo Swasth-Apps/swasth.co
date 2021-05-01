8a116eba037e79e8014fe8d7eb320c0e66566e75
vikas <vikasvaghasiya1212@gmail.com>
2021-05-01 23:26:09 +0530

    -- changes

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,15 +5,16 @@
 //  */
 //
 // // You can delete this file if you're not using it
-const _ = require('lodash')
-const path = require('path')
-const { createFilePath } = require('gatsby-source-filesystem')
-const { fmImagesToRelative } = require('gatsby-remark-relative-images')
+const _ = require('lodash');
+const path = require('path');
+const {createFilePath} = require('gatsby-source-filesystem');
+const {fmImagesToRelative} = require('gatsby-remark-relative-images');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 //
-exports.createPages = ({ actions, graphql }) => {
-  const { createPage } = actions;
+exports.createPages = ({actions, graphql}) => {
+    const {createPage} = actions;
 
-  return graphql(`{
+    return graphql(`{
      blogs:allMarkdownRemark(
           sort: { order: DESC, fields: [frontmatter___date] }
           filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
@@ -60,130 +61,143 @@ exports.createPages = ({ actions, graphql }) => {
       }
     }
   `).then(result => {
-    if (result.errors) {
-      result.errors.forEach(e => console.error(e.toString()))
-      return Promise.reject(result.errors)
+        if (result.errors) {
+            result.errors.forEach(e => console.error(e.toString()));
+            return Promise.reject(result.errors)
+        }
+
+        const posts = result.data.allMarkdownRemark.edges;
+
+        /******** for Infinite scroll in blog index page starts *********/
+        createPage({
+            path: "/blog",
+            component: path.resolve('src/templates/blog-index.js'),
+            context: {
+                blogList: result.data.blogs.edges,
+            },
+        });
+        /******** for Infinite scroll in blog index page ends *********/
+
+        posts.forEach(edge => {
+            const id = edge.node.id;
+            createPage({
+                path: edge.node.fields.slug,
+                tags: edge.node.frontmatter.tags,
+                component: path.resolve(
+                    `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
+                ),
+                // additional data can be passed via context
+                context: {
+                    id,
+                },
+            })
+        })
+
+        // // Tag pages:
+        // let tags = []
+        // // Iterate through each post, putting all found tags into `tags`
+        // posts.forEach(edge => {
+        //   if (_.get(edge, `node.frontmatter.tags`)) {
+        //     tags = tags.concat(edge.node.frontmatter.tags)
+        //   }
+        // })
+        // // Eliminate duplicate tags
+        // tags = _.uniq(tags)
+        //
+        // // Make tag pages
+        // tags.forEach(tag => {
+        //   const tagPath = `/tags/${_.kebabCase(tag)}/`
+        //
+        //   createPage({
+        //     path: tagPath,
+        //     component: path.resolve(`src/templates/blog-post.js`),
+        //     context: {
+        //       tag,
+        //     },
+        //   })
+        // })
+    })
+};
+
+exports.onCreatePage = ({page, actions}) => {
+    const {createPage} = actions;
+
+    /*  if(page.path.match(/^\/programs/)){
+        createPage({
+          path: "/programs",
+          matchPath: '/programs/!*',
+          component: path.resolve(`src/pages/programs.js`)
+        })
+      }*/
+
+
+    if (page.path.match(/^\/coaching/)) {
+        createPage({
+            path: "/coaching",
+            matchPath: '/coaching/*',
+            component: path.resolve(`src/pages/coaching/index.js`)
+        })
     }
 
-    const posts = result.data.allMarkdownRemark.edges;
-
-    /******** for Infinite scroll in blog index page starts *********/
-    createPage({
-      path: "/blog",
-      component: path.resolve('src/templates/blog-index.js'),
-      context: {
-        blogList: result.data.blogs.edges,
-      },
-    });
-    /******** for Infinite scroll in blog index page ends *********/
-
-    posts.forEach(edge => {
-      const id = edge.node.id;
-      createPage({
-        path: edge.node.fields.slug,
-        tags: edge.node.frontmatter.tags,
-        component: path.resolve(
-          `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
-        ),
-        // additional data can be passed via context
-        context: {
-          id,
-        },
-      })
-    })
-
-    // // Tag pages:
-    // let tags = []
-    // // Iterate through each post, putting all found tags into `tags`
-    // posts.forEach(edge => {
-    //   if (_.get(edge, `node.frontmatter.tags`)) {
-    //     tags = tags.concat(edge.node.frontmatter.tags)
-    //   }
-    // })
-    // // Eliminate duplicate tags
-    // tags = _.uniq(tags)
-    //
-    // // Make tag pages
-    // tags.forEach(tag => {
-    //   const tagPath = `/tags/${_.kebabCase(tag)}/`
-    //
-    //   createPage({
-    //     path: tagPath,
-    //     component: path.resolve(`src/templates/blog-post.js`),
-    //     context: {
-    //       tag,
-    //     },
-    //   })
-    // })
-  })
-};
-
-exports.onCreatePage = ({ page, actions }) => {
-  const { createPage } = actions;
-
-/*  if(page.path.match(/^\/programs/)){
-    createPage({
-      path: "/programs",
-      matchPath: '/programs/!*',
-      component: path.resolve(`src/pages/programs.js`)
-    })
-  }*/
+    if (page.path.match(/^\/coaching\/program/)) {
+        createPage({
+            path: "/coaching/program",
+            matchPath: '/coaching/program/*',
+            component: path.resolve(`src/pages/coaching/program/index.js`)
+        })
+    }
 
 
-  if(page.path.match(/^\/coaching/)){
-    createPage({
-      path: "/coaching",
-      matchPath: '/coaching/*',
-      component: path.resolve(`src/pages/coaching/index.js`)
-    })
-  }
+    if (page.path.match(/^\/resilify\/program/)) {
+        createPage({
+            path: "/resilify/program",
+            matchPath: '/resilify/program/*',
+            component: path.resolve(`src/pages/resilify/program.js`)
+        })
+    }
 
-  if(page.path.match(/^\/coaching\/program/)){
-    createPage({
-      path: "/coaching/program",
-      matchPath: '/coaching/program/*',
-      component: path.resolve(`src/pages/coaching/program/index.js`)
-    })
-  }
-
-
-  if(page.path.match(/^\/resilify\/program/)){
-    createPage({
-      path: "/resilify/program",
-      matchPath: '/resilify/program/*',
-      component: path.resolve(`src/pages/resilify/program.js`)
-    })
-  }
-
-  if(page.path.match(/^\/expert/)){
-    createPage({
-      path: "/expert",
-      matchPath: '/expert/*',
-      component: path.resolve(`src/pages/expert.js`)
-    })
-  }
+    if (page.path.match(/^\/expert/)) {
+        createPage({
+            path: "/expert",
+            matchPath: '/expert/*',
+            component: path.resolve(`src/pages/expert.js`)
+        })
+    }
 
 
-  if(page.path.match(/^\/resilify\/category/)){
-    createPage({
-      path: "/resilify/category",
-      matchPath: '/resilify/category/*',
-      component: path.resolve(`src/pages/resilify/category/index.js`)
-    })
-  }
+    if (page.path.match(/^\/resilify\/category/)) {
+        createPage({
+            path: "/resilify/category",
+            matchPath: '/resilify/category/*',
+            component: path.resolve(`src/pages/resilify/category/index.js`)
+        })
+    }
 
 };
 
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
-  // fmImagesToRelative(node) // convert image paths for gatsby images
+exports.onCreateNode = ({node, actions, getNode}) => {
+    const {createNodeField} = actions;
+    // fmImagesToRelative(node) // convert image paths for gatsby images
 
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
-    })
-  }
+    if (node.internal.type === `MarkdownRemark`) {
+        const value = createFilePath({node, getNode});
+        createNodeField({
+            name: `slug`,
+            node,
+            value,
+        })
+    }
+};
+
+exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
+    if (stage === 'build-javascript') {
+        const config = getConfig()
+        const miniCssExtractPlugin = config.plugins.find(
+            plugin => plugin.constructor.name === 'MiniCssExtractPlugin'
+        )
+        if (miniCssExtractPlugin) {
+            miniCssExtractPlugin.options.ignoreOrder = true
+        }
+        actions.replaceWebpackConfig(config)
+    }
 }
