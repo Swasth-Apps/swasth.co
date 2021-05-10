b4fe8bf0b7167f7ebf9c0985cb7d70acb4c08506
vikas <vikasvaghasiya1212@gmail.com>
2021-05-11 02:02:41 +0530

    -- Changes

--- src/aws-appsync-url.js
+++ src/aws-appsync-url.js
@@ -14,8 +14,15 @@ const graphQl_endPoints = {
       BUCKET_URL: "https://lifecoach-userfiles-mobilehub-332859420.s3.amazonaws.com/public/"
   },
   production: {
+/*
+    // Production Resilify Tracks
     RESILIFY_TRACKS: "https://pgcm3zihm5d4vnuulp3oo6muyq.appsync-api.us-east-1.amazonaws.com/graphql",
     TRACK_APIKEY: "da2-csl22c3qijad5fusk3tscxgyhy",
+*/
+
+// Dev Resilify Tracks
+      RESILIFY_TRACKS: "https://ciognbc4eraszaetlzqusuzbey.appsync-api.us-east-1.amazonaws.com/graphql",
+      TRACK_APIKEY: "da2-3hyo2q3rgrhjxcgtk33mc7e76i",
 
     COACHING_MARKETING: 'https://pweihk6hhvfwtnmbw42d43uecy.appsync-api.us-east-1.amazonaws.com/graphql',
     COACHING_API_KEY: "da2-iv4jsoxdtzh4xfsngcpncoqeny",
