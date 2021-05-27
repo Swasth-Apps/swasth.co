879c870db2c48db752a4255eff476d0f7d6cb57c
vikas <vikasvaghasiya1212@gmail.com>
2021-05-27 11:37:30 +0530

    -- Update Point url to production for coaching

--- src/aws-appsync-url.js
+++ src/aws-appsync-url.js
@@ -7,10 +7,10 @@ const graphQl_endPoints = {
       /*
           RESILIFY_TRACKS: "https://pgcm3zihm5d4vnuulp3oo6muyq.appsync-api.us-east-1.amazonaws.com/graphql",
           TRACK_APIKEY: "da2-csl22c3qijad5fusk3tscxgyhy",
-          COACHING_MARKETING: 'https://pweihk6hhvfwtnmbw42d43uecy.appsync-api.us-east-1.amazonaws.com/graphql',
-          COACHING_API_KEY: "da2-iv4jsoxdtzh4xfsngcpncoqeny",
-          BUCKET_URL: "https://lifecoach-userfiles-mobilehub-332859420.s3.amazonaws.com/public/"
       */
+    COACHING_MARKETING: 'https://pweihk6hhvfwtnmbw42d43uecy.appsync-api.us-east-1.amazonaws.com/graphql',
+    COACHING_API_KEY: "da2-iv4jsoxdtzh4xfsngcpncoqeny",
+    BUCKET_URL: "https://lifecoach-userfiles-mobilehub-332859420.s3.amazonaws.com/public/"
 
 
 
@@ -19,9 +19,9 @@ const graphQl_endPoints = {
     RESILIFY_TRACKS: "https://ciognbc4eraszaetlzqusuzbey.appsync-api.us-east-1.amazonaws.com/graphql",
     TRACK_APIKEY: "da2-3hyo2q3rgrhjxcgtk33mc7e76i",
 
-    COACHING_MARKETING: 'https://2iytwnhpanhevbcif4vtbgb5re.appsync-api.us-east-1.amazonaws.com/graphql',
+/*    COACHING_MARKETING: 'https://2iytwnhpanhevbcif4vtbgb5re.appsync-api.us-east-1.amazonaws.com/graphql',
     COACHING_API_KEY: "da2-a3lvko7r7jeivfhhcgj3dfcfpe",
-    BUCKET_URL: "https://coachingapp-userfiles-mobilehub-1299275089.s3.amazonaws.com/public/"
+    BUCKET_URL: "https://coachingapp-userfiles-mobilehub-1299275089.s3.amazonaws.com/public/"*/
 
 };
 
