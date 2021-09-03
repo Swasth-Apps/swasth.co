28913f5e8b1b95ef494ab4e66902481e7b8f3b66
vikas <vikasvaghasiya1212@gmail.com>
2021-09-03 19:31:22 +0530

    -- changes

--- src/components/Resilify/common/MetaTags.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export const MetaTags = (ogUrl,ogImage) => (<React.Fragment>
-    <meta property="og:type" content="article"/>
-<meta property="og:url" content={ogUrl}/>
-<meta property="og:image" content={ogImage}/>
-    </React.Fragment>
-)
--- src/pages/mi-coach.js
+++ src/pages/mi-coach.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Layout from '../components/Layout/layout';
 import {Helmet} from "react-helmet";
 import MICoach from "../components/MICoach";
-import {MetaTags} from "../components/Resilify/common/MetaTags";
 
 class MICoachIndex extends React.Component {
     render() {
@@ -15,7 +14,9 @@ class MICoachIndex extends React.Component {
                         name="keywords"
                         content="MI Coach, Motivational Interviewing, Behavior Change, Behavior Tracking, Depression, Anxiety, Self Harm, Eating Disorders, Recovery, Anger, Relationships, PTSD, Chronic illness, OCD, PTSD, ADHD, Substance Abuse, Addiction, Stress"
                     />
-                    {MetaTags("https://resiliens.com/mi-coach/", "https://res.cloudinary.com/dbt/image/upload/v1630677105/resiliens/mi_og.png")}
+                    <meta property="og:type" content="article"/>
+                    <meta property="og:url" content="https://resiliens.com/mi-coach/"/>
+                    <meta property="og:image" content="https://res.cloudinary.com/dbt/image/upload/v1630677105/resiliens/mi_og.png"/>
                 </Helmet>
                 <div
                     id='wrapper'
