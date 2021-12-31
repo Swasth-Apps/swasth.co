a8473cb877c679ab15b6a6379bada8e0f2966f8c
vikas <vikasvaghasiya1212@gmail.com>
2021-12-31 20:13:42 +0530

    -- Remove appsflyer script

--- src/html.js
+++ src/html.js
@@ -49,13 +49,13 @@ export default class HTML extends React.Component {
                 <meta name="facebook-domain-verification" content="74eylemvkal03g10s7haulm4uc8jih" />
                 <meta name="theme-color" content="#ffffff"/>
 
-                <script dangerouslySetInnerHTML= {{ __html:`
+{/*                <script dangerouslySetInnerHTML= {{ __html:`
                 !function (t, e, n, s, a, c, i, o, p) {
                         t.AppsFlyerSdkObject = a, t.AF = t.AF || function () {
                             (t.AF.q = t.AF.q || []).push([Date.now()].concat(Array.prototype.slice.call(arguments)))
                         }, t.AF.id = t.AF.id || i, t.AF.plugins = {}, o = e.createElement(n), p = e.getElementsByTagName(n)[0], o.async = 1, o.src = "https://websdk.appsflyer.com?" + (c.length > 0 ? "st=" + c.split(",").sort().join(",") + "&" : "") + (i.length > 0 ? "af_id=" + i : ""), p.parentNode.insertBefore(o, p)
                     }(window, document, "script", 0, "AF", "banners", {banners: {key: "4213623e-8b49-4d3c-8d51-a14a2712ca1a"}})
-                        AF('banners', 'showBanner')`}} />
+                        AF('banners', 'showBanner')`}} />*/}
             </head>
             <body className="h-full" style={{height: "100%"}}>
             {this.props.preBodyComponents}
